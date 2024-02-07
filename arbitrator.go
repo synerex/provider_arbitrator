b2e3872ae6ba8e8ed3d8279d2dab7ae96c2cf690
Yoshiteru Nagata <yositeru1108@nagoya-u.jp>
2024-02-07 20:20:13 +0900

    rename

--- arbitrator.go
+++ arbitrator.go
@@ -19,9 +19,10 @@ import (
 var (
 	nodesrv         = flag.String("nodesrv", "127.0.0.1:9990", "Node ID Server")
 	local           = flag.String("local", "", "Local Synerex Server")
+	num             = flag.Int("num", 1, "Number of Arbitrator")
 	mu              sync.Mutex
 	version         = "0.0.0"
-	role            = "BusOperator"
+	role            = "Arbitrator"
 	sxServerAddress string
 )
 
@@ -83,12 +84,12 @@ func reconnectClient(client *sxutil.SXServiceClient) {
 func main() {
 	go sxutil.HandleSigInt()
 	sxutil.RegisterDeferFunction(sxutil.UnRegisterNode)
-	log.Printf("%s(%s) built %s sha1 %s", role, sxutil.GitVer, sxutil.BuildTime, sxutil.Sha1Ver)
+	log.Printf("%s%d(%s) built %s sha1 %s", role, num, sxutil.GitVer, sxutil.BuildTime, sxutil.Sha1Ver)
 
 	channelTypes := []uint32{pbase.ALT_PT_SVC, pbase.JSON_DATA_SVC}
 
 	var rerr error
-	sxServerAddress, rerr = sxutil.RegisterNode(*nodesrv, role, channelTypes, nil)
+	sxServerAddress, rerr = sxutil.RegisterNode(*nodesrv, fmt.Sprintf("%s%d", role, num), channelTypes, nil)
 
 	if rerr != nil {
 		log.Fatal("Can't register node:", rerr)
@@ -108,8 +109,8 @@ func main() {
 		log.Print("Connecting SynerexServer")
 	}
 
-	rcmClient := sxutil.NewSXServiceClient(client, pbase.ALT_PT_SVC, fmt.Sprintf("{Client:%s}", role))
-	envClient := sxutil.NewSXServiceClient(client, pbase.JSON_DATA_SVC, fmt.Sprintf("{Client:%s}", role))
+	rcmClient := sxutil.NewSXServiceClient(client, pbase.ALT_PT_SVC, fmt.Sprintf("{Client:%s%d}", role, num))
+	envClient := sxutil.NewSXServiceClient(client, pbase.JSON_DATA_SVC, fmt.Sprintf("{Client:%s%d}", role, num))
 
 	wg.Add(1)
 	log.Print("Subscribe Supply")
